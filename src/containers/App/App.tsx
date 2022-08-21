7abc8abba389fa1a30e13cce5f502006381185d2
rohit <b805rohit@gmail.com>
2022-08-21 12:44:44 +0530

    Init:App Setup

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,17 @@
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
+import './App.css';
+
+function App() {
+
+  return (
+    <Router>
+      <section className="app-wrapper">
+        
+      </section>
+    </Router>
+  )
+}
+
+export default App
